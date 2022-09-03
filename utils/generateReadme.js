45818913a2324188185018c9c21633aab3ebc02d
Patrick Richardson Wright <wrightmight@gmail.com>
2022-09-02 21:34:15 -0700

    remove whitespace before md lines

--- utils/generateReadme.js
+++ utils/generateReadme.js
@@ -12,48 +12,48 @@ const generateReadme = ({
   contact,
 }) => {
   return `
-    ## ${title}
-    ![badge](https://img.shields.io/badge/license-${license}-brightgreen)
+## ${title}
+![badge](https://img.shields.io/badge/license-${license}-brightgreen)
 
-    - [Description](#description)
-    - [Installation](#installation)
-    - [Usage](#usage)
-    - [Contributing](#contribution)
-    - [Tests](#tests)
-    - [Resources & Inspiration](#resources-inspiration)
-    - [Question](#questions)
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contribution)
+- [Tests](#tests)
+- [Resources & Inspiration](#resources-inspiration)
+- [Question](#questions)
 
-    # Description 
-    Motivation: ${description.motivation}<br />
-    The 'why' behind this app: ${description.why}<br />
-    What is being solved: ${description.solve}<br />
-    What was learned during app development: ${description.learn}
+# Description 
+Motivation: ${description.motivation}<br />
+The 'why' behind this app: ${description.why}<br />
+What is being solved: ${description.solve}<br />
+What was learned during app development: ${description.learn}
 
-    # Installation
-    ${installation}
+# Installation
+${installation}
 
-    # Usage
-    ${usage}
+# Usage
+${usage}
 
-    # License
-    ![badge](https://img.shields.io/badge/license-${license}-brightgreen)
-    <br />
-    This application is covered by the ${license} license. 
+# License
+![badge](https://img.shields.io/badge/license-${license}-brightgreen)
+<br />
+This application is covered by the ${license} license. 
 
-    # Contributing
-    ${contribute}
+# Contributing
+${contribute}
 
-    # Tests
-    ${test}
+# Tests
+${test}
 
-    # Resources & Inspiration
-    Packages used in this app: ${attribution}<br />
-    Tutorials followed when creating this app: ${tutorial}
+# Resources & Inspiration
+Packages used in this app: ${attribution}<br />
+Tutorials followed when creating this app: ${tutorial}
 
-    # Questions?
-    Find me on GitHub: [${contact.githubUser}](${contact.githubUser})<br />
-    Email me at [${contact.email}](mailto:${contact.email})
-    `;
+# Questions?
+Find me on GitHub: [${contact.githubUser}](${contact.githubUser})<br />
+Email me at [${contact.email}](mailto:${contact.email})
+`;
 };
 
 module.exports = generateReadme;
